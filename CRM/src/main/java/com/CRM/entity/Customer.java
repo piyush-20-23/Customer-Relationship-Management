f0a4c0c50dc9f9f070800ce6ad2853004961c40b
piyush-20-23 <patlepiyush382@gmail.com>
2026-06-12 01:20:07 +0530

    Entity class created

--- CRM/src/main/java/com/CRM/entity/Customer.java
+++ CRM/src/main/java/com/CRM/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.CRM.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -7,18 +8,60 @@ import jakarta.persistence.Table;
 @Entity
 @Table
 public class Customer {
+	
+	public Customer() {
+	}
 
 	@Id
+	@Column(name = "cust_id")
 	private int id;
 	
+	@Column(name = "first_name")
 	private String firstName;
 	
+	@Column(name = "last_name")
 	private String lastName;
 	
+	@Column(name = "email")
 	private String email;
 	
+	@Column(name = "phone_number")
 	private String phoneNumber;
 	
+	@Column(name = "age")
 	private int age;
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	
 }
